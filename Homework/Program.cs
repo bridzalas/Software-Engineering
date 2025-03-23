015ed429b3678df2d43cbe2918b81b9b9116ae0e
Bridzalas <brigita.linka000@gmail.com>
2025-03-23 08:12:57 +0200

    Initial commit

---
+++ Homework/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence,newSentence, word, newWord;
+            Console.WriteLine("Write any sentence.");
+            sentence=Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Write a word that you want to replace.");
+            word = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Write a new word that you want to replace that last word with.");
+            newWord = Console.ReadLine();
+            Console.WriteLine();
+            newSentence = sentence.Replace(word, newWord);
+            if(newSentence==sentence)
+            {
+                Console.WriteLine("The sentence remains the same, the word that you entered doesn't exist in your sentence.");
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Sentence after replacing word '{0}' with word '{1}'", word, newWord);
+                Console.WriteLine(newSentence);
+                Console.WriteLine();
+            }
+            
+
+
+        }
+    }
+}
